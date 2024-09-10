f64dbbe4fb77d553409b0212fa4149b94b8a7bc1
LamPhamNgoc1310 <phamngoclam1310@gmail.com>
2024-09-10 13:52:51 +0700

    docs: add electron+react template, instruction in README.md

--- usthproject/index.js
+++
@@ -1,8 +0,0 @@
-button = document.getElementsByClassName("button")[0];
-text = document.getElementsByClassName("text")[0];
-
-function displayText() {
-    text.innerHTML = "Me may dep:)"
-}
-
-button.addEventListener('click', displayText);
